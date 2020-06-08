c7697068c3d1251980c56208679d7ea3b39816fb
Rufus Wong <rcywongaa@gmail.com>
2020-06-08 12:11:01 +0800

    Fix setup.bash script

--- setup.bash
+++ setup.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+
+sudo apt update
+
 sudo apt install \
     python-catkin-tools
