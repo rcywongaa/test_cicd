7e697352730b6438138bf15bf3e8b6f3b9f37e3d
Rufus Wong <rcywongaa@gmail.com>
2020-06-08 12:16:31 +0800

    Add --no-check-certificate to fix no valid openPGP data found

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+wget --no-check-certificate http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
 sudo apt update
 
