54393bb1eb75e76f838b4b910d13e2fdd87efac9
Rufus Wong <rcywongaa@gmail.com>
2020-06-08 12:23:11 +0800

    Install wget

--- setup.bash
+++ setup.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+sudo apt install wget
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget --no-check-certificate http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
 sudo apt update
 
-sudo apt install \
+sudo apt install -y \
     python-catkin-tools
