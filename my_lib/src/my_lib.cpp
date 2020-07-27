6e239cd3de326ad310de7a4afb5a7e23764c09fd
Rufus Wong <rcywongaa@gmail.com>
2020-07-27 16:16:11 +0800

    Test failure

--- my_lib/src/my_lib.cpp
+++ my_lib/src/my_lib.cpp
@@ -1,6 +1,10 @@
 #include "my_lib/my_lib.hpp"
+#include <memory>
 
 int return_one()
 {
-    return 1;
+    //std::unique_ptr<int> a = std::make_unique<int>(3);
+    //std::unique_ptr<int> b = std::move(a);
+    //*a;
+    return 0;
 }
